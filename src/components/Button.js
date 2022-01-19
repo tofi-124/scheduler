75a1f471deab0deced4cb0fd2b709254b6f776bf
tofi-124 <tofikahm@yahoo.com>
2022-01-19 13:25:36 -0500

    Building a Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,26 @@ import React from "react";
 
 import "components/Button.scss";
 
+//Returns buttons view
 export default function Button(props) {
-   return <></>;
+
+  let buttonClass = "button";  
+
+  if(props.confirm){
+    buttonClass += " button--confirm";
+  }
+
+  else if(props.danger){
+    buttonClass += " button--danger";
+  }
+
+  return (
+  <button 
+    className={buttonClass} 
+    onClick = {props.onClick}
+    disabled = {props.disabled}
+  >
+    {props.children}
+  </button>
+  )
 }
