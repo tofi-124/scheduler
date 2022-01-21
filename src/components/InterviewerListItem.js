4bb1c7f2937f241b4cde3a3d389d24407a3b3733
tofi-124 <tofikahm@yahoo.com>
2022-01-21 14:16:56 -0500

    InterviewerListItem implmentation

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,18 +1,22 @@
 import React from "react";
-import cs from "classnames";
 import "components/InterviewerListItem.scss";
+import classNames from "classnames";
 
-export default function (
-  { name, avatar, selected, setInterviewer } = this.props
-) {
-  const interviewerClass = cs("interviewers__item", {
-    "interviewers__item--selected": selected,
-  });
-
+export default function InterviewerListItem(props) {
   return (
-    <li className={interviewerClass} onClick={setInterviewer} data-testid="day">
-      <img className={"interviewers__item-image"} src={avatar} alt={name} />
-      {selected && name}
+    <li
+      className={classNames("interviewers__item", {
+        "interviewers__item--selected": props.selected,
+      })}
+      selected={props.selected}
+      onClick={props.setInterviewer}
+    >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
     </li>
   );
 }
