b3a95683477e0dce1f9597fb1c435c9f49f6c426
tofi-124 <tofikahm@yahoo.com>
2022-01-21 14:52:02 -0500

    Button component updated

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,27 +1,20 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
-//Returns buttons view
 export default function Button(props) {
-
-  let buttonClass = "button";  
-
-  if(props.confirm){
-    buttonClass += " button--confirm";
-  }
-
-  else if(props.danger){
-    buttonClass += " button--danger";
-  }
+  let buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger,
+  });
 
   return (
-  <button 
-    className={buttonClass} 
-    onClick = {props.onClick}
-    disabled = {props.disabled}
-  >
-    {props.children}
-  </button>
-  )
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
