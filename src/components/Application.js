c7d7ec3413461da2136834f853aba09f6e8237db
tofi-124 <tofikahm@yahoo.com>
2022-01-21 14:50:56 -0500

    impln of appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,9 +2,48 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+  {
+    id: 5,
+    time: "4pm",
+  },
+];
 
 export default function Application(props) {
-
   const days = [
     {
       id: 1,
@@ -24,7 +63,7 @@ export default function Application(props) {
   ];
 
   const [day, setDay] = useState("Monday");
-  
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -35,7 +74,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={days} day={day} setDay={(day) => console.log(day)} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -45,6 +84,10 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map((app) => {
+          return <Appointment key={app.id} {...app} />;
+        })}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
