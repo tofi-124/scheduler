201ea87b69ddd7df2285bea3d77c2014e69fd4f9
tofi-124 <tofikahm@yahoo.com>
2022-01-22 00:09:31 -0500

    importing useVisualMode from hooks and updating the code to use hooks

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
 
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
-          interviewer={props.interview.interviewer.name}
+          interviewer={props.interview.interviewer}
         />
-      ) : (
-        <Empty />
+      )}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && (
+        <Form interviewers={props.interviewers} onCancel={() => back(EMPTY)} />
       )}
     </article>
   );
