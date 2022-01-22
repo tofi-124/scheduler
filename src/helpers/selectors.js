9493264ed89d49b93c563537e4d63131f194cbf3
tofi-124 <tofikahm@yahoo.com>
2022-01-21 23:50:57 -0500

    tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,8 +1,28 @@
 export function getAppointmentsForDay(state, day) {
   const selectedDay = state.days.filter((singleDay) => singleDay.name === day);
   let appointments = [];
+  
   if (selectedDay[0] && selectedDay[0].appointments) {
     appointments = selectedDay[0].appointments;
   }
   return appointments.map((id) => state.appointments[id]);
+}
+
+export function getInterview(state, interview) {
+  if (!interview) return null;
+  const { student, interviewer } = interview;
+  return {
+    student,
+    interviewer: state.interviewers[interviewer],
+  };
+}
+
+export function getInterviewersForDay(state, day) {
+  const selectedDay = state.days.filter((singleDay) => singleDay.name === day);
+  let dayInterviewers = [];
+  
+  if (selectedDay[0] && selectedDay[0].interviewers) {
+    dayInterviewers = selectedDay[0].interviewers;
+  }
+  return dayInterviewers.map((id) => state.interviewers[id]);
 }
