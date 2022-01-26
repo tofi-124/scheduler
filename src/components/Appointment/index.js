8946569bb504ead780cdec0f1e478bca7f07ae98
tofi-124 <tofikahm@yahoo.com>
2022-01-26 14:01:00 -0500

    Tests

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -46,7 +46,7 @@ export default function Appointment(props) {
   }
 
   return (
-    <article className="appointment">
+    <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
       {mode === SAVE && <Status message="Saving" />}
       {mode === DELETE && <Status message="Deleting" />}
